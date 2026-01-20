bbd4d81fa41f451ca55862650100f303ae9aa308
Andrew Monty <andrew1503@gmail.com>
2026-01-19 21:12:14 -0500

    tweak sidebar

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,8 +1,11 @@
+import AppLogoIcon from "./app-logo-icon";
+
 export default function AppLogo() {
     return (
         <>
-            <div className="font-mono text-lg font-semibold">
-                <span className="truncate leading-tight">AndrewOS</span>
+            <div className="flex items-center gap-2 font-mono text-lg font-semibold">
+                <AppLogoIcon />
+                <span className="truncate">AndrewOS</span>
             </div>
         </>
     );
